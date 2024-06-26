ca5b70f623e4eb5101d853d7c98e2764408e6fe9
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-06-25 22:46:37 -0300

    feat: adding register

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import fastify, { FastifyInstance } from "fastify";
+import { userRoutes } from './routes/user.routes'
+import { contactsRoutes } from "./routes/contact.routes";
 
 const app: FastifyInstance = fastify();
+
+app.register(userRoutes, {
+    prefix: '/users',
+});
+
+app.register(contactsRoutes, {
+    prefix: '/contacts',
+});
 
 app.listen(
     {
